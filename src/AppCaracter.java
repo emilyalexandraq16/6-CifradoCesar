063382a030c18cbfe901c544031e75fb06c1ccb7
Kat-01\Emily <emilyalexandraq16@gmail.com>
2026-04-28 21:56:51 +0200

    Se ha añadido un condicional if para verificar que el numero de desplazamiento entra dentro del rango permitido y un bucle do para verificar si la ruta absoluta existe

--- src/AppCaracter.java
+++ src/AppCaracter.java
@@ -9,7 +9,7 @@ public class AppCaracter {
     public static void main(String[] args) throws Exception {
 
         Scanner sc= new Scanner(System.in);
-        System.out.println("Vamos a codificar un texto. ¿Que archivo te gustaria codificar? (Debe estar dentro de src/ficheros)");
+        System.out.println("Vamos a codificar un texto. ¿Que archivo te gustaria codificar? (Debe estar dentro de src/ficheros): ");
         String archivo= sc.nextLine();
         File ficheroEntrada= new File("src/ficheros/" + archivo);
 
@@ -18,26 +18,35 @@ public class AppCaracter {
                 System.out.println("Este fichero no es válido, vuelva a intentarlo: ");
                 archivo= sc.nextLine();
                 ficheroEntrada = new File("src/ficheros/" + archivo);
-            }
-            
+            }            
         } while (!ficheroEntrada.exists());
 
-        System.out.println("Introduzca el desplazamiento, desde -25 a 25: ");
+        System.out.println("Introduzca el nº de desplazamiento, desde -25 a 25: ");
         int desplazamiento= sc.nextInt();
         sc.nextLine();
+
+        if(desplazamiento>= -25 && desplazamiento<=25) {
+            System.out.println("Preparando archivo...");
+        } else {
+            System.out.println("ERROR, el nº de desplazamiento debe estar entre -25 y 25.");
+        } while (desplazamiento < -25 && desplazamiento > 25) {
+            System.out.println("ERROR, el nº de desplazamiento debe estar entre -25 y 25.");
+        }
 
 
         System.out.println("Introduce la ruta absoluta para el archivo de salida");
         String rutaSalida= sc.nextLine();
         File ficheroSalida = new File(rutaSalida);
 
-        // Abrimos el archivo para leerlo
-        // ALTERNATIVA: FileReader lector = new FileReader("entrada.txt");
-        FileReader lector = new FileReader(ficheroEntrada);
+        do {
+            if (ficheroSalida.exists()) {
+                System.out.println("ERROR, este archivo ya existe. Introduzca otra ruta absoluta: ");
+                rutaSalida=sc.nextLine();
+                ficheroSalida=new File(rutaSalida);
+            }
+        } while (ficheroSalida.exists());
 
-        // Creamos el archivo de salida
-        // SI NO EXISTE, LO CREA
-        // ALTERNATIVA: FileWriter escritor = new FileWriter("salida.txt");
+        FileReader lector = new FileReader(ficheroEntrada);
         FileWriter escritor = new FileWriter(ficheroSalida);
 
         // Se va leyendo del fichero CARACTER A CARACTER
@@ -52,14 +61,7 @@ public class AppCaracter {
             char caracter = (char) caracterInt;
 
             // Mostramos por pantalla el caracter procesado
-            System.out.println(caracter);
-
-            /*
-                 * Escribimos en el fichero de salida.
-                 * Se recomienda (si es posible) no almacenar todo el
-                 * contenido del fichero en un String, ya que desconocemos
-                 * su tamaño (puede ser de mucho tamaño)
-             */
+            System.out.print(caracter);
             escritor.write(caracter);
         }
 
